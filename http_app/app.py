c8dc1b5070c543ab4624f73057e60ef736acaf83
Senthil Appasamy <senthil.ar@rakuten.com>
2022-03-08 16:03:14 +0800

    Initial commit

---
+++ http_app/app.py
@@ -0,0 +1,37 @@
+#!env python3
+from flask import Flask, request
+import logging
+from argparse import *
+
+log = logging.getLogger('werkzeug')
+logging.basicConfig(level=logging.ERROR,format='%(asctime)s  Request-URI: %(message)s')
+log.setLevel(logging.ERROR)
+
+### arg_parser  ###
+def arg_parser():
+    parser = ArgumentParser()
+    parser.add_argument('-d', '--debug', help='debug flag',action='store_true',required=False)
+    args = parser.parse_args()
+    global flag
+    if args.debug:
+       flag=True
+    else:
+       flag=False
+
+
+def init_app():
+  app = Flask(__name__)
+  @app.route('/', defaults={'path': ''},methods=['GET','POST'])
+  @app.route('/<path:path>',methods=['GET','POST'])
+  def echo(path):
+    if request.headers.get('Accept') == 'application/json':
+        app.logger.debug(request.path)
+        return({"message": "Hello, World"})
+    else:
+        return('<p>Hello, World</p>')
+  return app
+
+if __name__ == '__main__':
+    arg_parser()
+    app = init_app()
+    app.run(debug=flag, host='0.0.0.0', port=5000)
---
+++ tests/test_get_post.py
@@ -0,0 +1,30 @@
+# Third party modules
+import pytest
+
+# First party modules
+from http_app.app import init_app
+
+
+@pytest.fixture
+def client():
+    app = init_app()
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_simple_get(client):
+    rv = client.get("/")
+    assert b"<p>Hello, World</p>" == rv.data
+
+def test_json_get(client):
+    rv = client.get("/",headers={"Accept": "application/json"})
+    assert b'{"message":"Hello, World"}\n' == rv.data
+
+def test_simple_post(client):
+    rv = client.post("/")
+    assert b"<p>Hello, World</p>" == rv.data
+
+def test_json_post(client):
+    rv = client.post("/",headers={"Accept": "application/json"})
+    assert b'{"message":"Hello, World"}\n' == rv.data
